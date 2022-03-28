1bf532afc758c3e4068370b53e1745bfc64578bd
Waju <lanre.ajibua@gmail.com>
2022-03-28 23:56:52 +0200

    Tour & Tour log blueprints

---
+++ src/main/java/com/example/tourplanner/models/Tour.java
@@ -0,0 +1,14 @@
+package com.example.tourplanner.models;
+
+public class Tour {
+    private String tourId;
+    private String tourName;
+    private String tourDescription;
+    private String from;
+    private String to;
+    private String transportType;
+    private float distance;
+    private int estimatedTime;
+    private String routeInformationImageURL;
+
+}
---
+++ src/main/java/com/example/tourplanner/models/TourLog.java
@@ -0,0 +1,11 @@
+package com.example.tourplanner.models;
+
+import java.time.LocalDate;
+
+public class TourLog {
+    private String tourLogId;
+    private LocalDate date;
+    private String difficulty;
+    private float totalTime;
+    private Tour tour;
+}
