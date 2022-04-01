73f2078ed5b0b6d3ae55930242570de49adce7f4
Tom Mihnjak <if20b277@technikum-wien.at>
2022-04-01 16:49:50 +0200

    JavaFX Design added

--- src/main/java/com/example/tourplanner/Main.java
+++ src/main/java/com/example/tourplanner/Main.java
@@ -1,8 +1,10 @@
 package com.example.tourplanner;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Tour Planner");
         stage.setScene(scene);
         stage.show();
