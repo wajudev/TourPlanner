3e73fd12e0d4964cbdeba223cde4f6e0da72279d
Waju <lanre.ajibua@gmail.com>
2022-05-28 14:18:45 +0200

    Charts to avoid deletion after branch change

--- src/main/java/com/example/tourplanner/views/ChartsViewController.java
+++ src/main/java/com/example/tourplanner/views/ChartsViewController.java
@@ -1,4 +1,14 @@
 package com.example.tourplanner.views;
 
-public class ChartsViewController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChartsViewController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
