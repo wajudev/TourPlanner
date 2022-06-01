6519dd0260c8df65905951aa1aded190a93cd4a6
Waju <lanre.ajibua@gmail.com>
2022-06-01 16:13:19 +0200

    Bild in Report

--- src/main/java/com/example/tourplanner/business/report/Report.java
+++ src/main/java/com/example/tourplanner/business/report/Report.java
@@ -5,12 +5,15 @@ import com.example.tourplanner.business.app.TourManagerImpl;
 import com.example.tourplanner.models.Tour;
 import com.example.tourplanner.models.TourLog;
 import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.font.PdfFont;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.element.Text;
@@ -101,9 +104,12 @@ public class Report {
             table.addCell(new Paragraph(tourLog.getComment()).setFont(fontItalic));
         }
 
-        System.out.println("test1");
-        System.out.println(tour.getRouteInformationImageURL());
-        System.out.println("tets2");
+        String url = tour.getRouteInformationImageURL();
+        ImageData imageData = ImageDataFactory.create(url);
+
+        Image image = new Image(imageData);
+
+        document.add(image);
 
         document.add(table);
         document.close();
